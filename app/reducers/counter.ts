20a477fdd8c257ba6f19a19afca506d997a2661a
Chris Feijoo <christophe@feijoo.fr>
2016-10-16 21:34:29 +0200

    Use when-switch for conciseness

--- app/reducers/counter.ts
+++ app/reducers/counter.ts
@@ -1,5 +1,6 @@
 import { set } from 'monolite'
 import { Action, Reducer } from 'redux'
+import when from 'when-switch'
 
 export type CounterState = {
   value: number
@@ -16,15 +17,10 @@ const decrement = (state: CounterState) =>
   set(state, _ => _.value)(value => value - 1)
 
 const counterReducer: Reducer<CounterState> =
-  (state = INITIAL_STATE, action: Action) => {
-    switch (action.type) {
-      case 'INCREMENT':
-        return increment(state)
-      case 'DECREMENT':
-        return decrement(state)
-      default:
-        return state
-    }
-  }
+  (state = INITIAL_STATE, action: Action) =>
+    when(action.type)
+      .is('INCREMENT', () => increment(state))
+      .is('DECREMENT', () => decrement(state))
+      .else(state)
 
 export default counterReducer
