86ff9b90bfd82771cbc9c2b8449cef8d9d726dfa
Chris Feijoo <christophe@feijoo.fr>
2016-11-14 00:11:08 +0100

    Fix relative require of `../reducers`

--- app/store/configureStore.prod.ts
+++ app/store/configureStore.prod.ts
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import * as createLogger from 'redux-logger'
-import rootReducer, { State } from '../reducers'
+
+import rootReducer, { State } from 'reducers'
 
 const loggerMiddleware = createLogger()
 const enhancer = applyMiddleware(thunkMiddleware, loggerMiddleware)
