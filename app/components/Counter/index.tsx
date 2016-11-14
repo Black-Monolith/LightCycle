c0e1ed778f5b16dcfde2df1e536cd8d511765143
Chris Feijoo <christophe@feijoo.fr>
2016-11-14 15:29:09 +0100

    Simplify Shortcut component and include it in Home

--- app/components/Counter/index.tsx
+++ app/components/Counter/index.tsx
@@ -1,15 +1,31 @@
 import * as React from 'react'
 import * as style from './Counter.scss'
+import { CounterState } from 'reducers/counter'
 
-type Props = {
-  counter: number
+export type MappedState = CounterState
+
+export type MappedDispatch = {
+  increment: React.EventHandler<any>,
+  decrement: React.EventHandler<any>,
+  incrementAsync: React.EventHandler<any>
 }
+
+type Props = CounterState & MappedDispatch
 
 export default (props: Props) => (
   <div>
     <h1>Counter.</h1>
     <p className={style['counter']}>
-      {props.counter}
+      {props.value}
     </p>
+    <button onClick={props.increment}>
+      Increment
+    </button>
+    <button onClick={props.decrement}>
+      Decrement
+    </button>
+    <button onClick={props.incrementAsync}>
+      IncrementAsync
+    </button>
   </div>
 )
--- app/components/Home/index.tsx
+++ app/components/Home/index.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react'
-import Shortcut from 'components/Shortcut'
+import * as style from './Home.scss'
+
+const Shortcut = (props: { binding: string }) => (
+  <span className={style['shortcut']}>
+    {props.binding}
+  </span>
+)
 
 export default () => (
   <div>
     <h1>Hello.</h1>
     <p>Welcome to LightCycle.</p>
-    <p>Use <Shortcut ctrl letter='H' /> to display Redux Devtools.</p>
-    <p>Use <Shortcut ctrl letter='P' /> to change Devtools position.</p>
+    <p>Use <Shortcut binding='Ctrl+H' /> to display Redux Devtools.</p>
+    <p>Use <Shortcut binding='Ctrl+P' /> to change Devtools position.</p>
   </div>
 )
