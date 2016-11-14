6daef7363dd773e4c1047d69bacac37f1784a597
Chris Feijoo <christophe@feijoo.fr>
2016-11-14 13:57:07 +0100

    Clean configureStore index

--- app/store/index.ts
+++ app/store/index.ts
@@ -3,12 +3,7 @@ import { State } from 'reducers'
 
 type ConfigureStore = (initialState?: State) => Store<State>
 
-let configureStore: ConfigureStore
-
-if (process.env.NODE_ENV === 'production') {
-  configureStore = require<ConfigureStore>('./configureStore.prod')
-} else {
-  configureStore = require<ConfigureStore>('./configureStore.dev')
-}
-
-export default configureStore
+export default
+  process.env.NODE_ENV === 'production' ?
+    require<ConfigureStore>('./configureStore.prod') :
+    require<ConfigureStore>('./configureStore.dev')
