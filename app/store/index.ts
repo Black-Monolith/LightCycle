a498d0403a8cb137346a9161a074fc2508911cc0
Chris Feijoo <christophe@feijoo.fr>
2017-04-27 18:59:33 +0200

    Use single file for Store

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,9 +1,32 @@
-import { Store } from 'redux'
-import { State } from 'reducers'
+import { createStore, applyMiddleware, compose, StoreEnhancer } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
 
-type ConfigureStore = (initialState?: State) => Store<State>
+import rootReducer, { State } from 'reducers'
+import DevTools from 'containers/DevTools'
 
-export default
-  process.env.NODE_ENV === 'production' ?
-    require<ConfigureStore>('./configureStore.prod') :
-    require<ConfigureStore>('./configureStore.dev')
+const enhancer =
+  process.env.NODE_ENV === 'production'
+    ? compose(
+      applyMiddleware(
+        thunkMiddleware
+      )
+    ) as StoreEnhancer<State>
+    : compose(
+      applyMiddleware(
+        thunkMiddleware,
+        createLogger()
+      ),
+      DevTools.instrument()
+    ) as StoreEnhancer<State>
+
+export default (initialState?: State) => {
+  const store = createStore<State>(rootReducer, initialState, enhancer)
+
+  if (module.hot) {
+    module.hot.accept('reducers', () =>
+      store.replaceReducer(require<typeof rootReducer>('reducers'))
+    )
+  }
+  return store
+}
